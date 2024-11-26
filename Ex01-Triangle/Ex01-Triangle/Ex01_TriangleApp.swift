5db66e9dc71cb096ff6390840bc911bcbbe9ec6b
B0X <83914919+BOLTB0X@users.noreply.github.com>
2024-11-26 18:31:51 +0900

    정리

---
+++ Ex01-Triangle/Ex01-Triangle/Ex01_TriangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ex01_TriangleApp.swift
+//  Ex01-Triangle
+//
+//  Created by KyungHeon Lee on 2024/11/26.
+//
+
+import SwiftUI
+
+@main
+struct Ex01_TriangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
